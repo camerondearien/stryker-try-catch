c33a9065a7f649e3cd92ac1cbff33101c108d3f3
camerondearien <46691836+camerondearien@users.noreply.github.com>
2022-01-04 09:42:46 -0800

    fixed issue with tests not running

--- unit-tests/tests/SampleTests.cs
+++ unit-tests/tests/SampleTests.cs
@@ -8,12 +8,19 @@ namespace unit_tests
     public class SampleTests
     {
         [Fact]
-        public async Task Just_A_Sample_Test()
+        public async Task Returns_a_number()
         {
             var number = Sample.TestMe(5);
 
             number.Should().Be(5);
         }
 
+        [Fact]
+        public async Task Throws_exception()
+        {
+            var number = Sample.TestMe(0);
+
+            number.Should().Be(-1);
+        }
     }
 }
