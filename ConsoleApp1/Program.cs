5198e886585341bb74503af4868254733b6e716a
Cameron Dearien <cameron_dearien@selinc.com>
2022-01-04 08:36:27 -0800

    Initial commit with simple test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public static class Sample
+    {
+        public static int TestMe(int number)
+        {
+            try
+            {
+                if(number == 0)
+                {
+                    throw new Exception("Throwing exception.");
+                }
+
+                return number;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception Message: {ex.Message}");
+                return -1;
+            }
+        }
+    }
+}
---
+++ unit-tests/tests/SampleTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using System.Threading.Tasks;
+using Xunit;
+using ConsoleApp1;
+
+namespace unit_tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public async Task Just_A_Sample_Test()
+        {
+            var number = Sample.TestMe(5);
+
+            number.Should().Be(5);
+        }
+
+    }
+}
